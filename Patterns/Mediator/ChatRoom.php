1564bf9ffc276aeb636423c9523080ef0ac7109e
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-04-10 19:42:39 +0700

    Change desplay message structure

--- Patterns/Mediator/ChatRoom.php
+++ Patterns/Mediator/ChatRoom.php
@@ -2,7 +2,10 @@
 include_once ("User.php");
 class ChatRoom {
    public static function showMessage(User $user, $message){
-      print_r(date("H:i:s"). " [".$user->getName()."] : ".$message."\n");
+      print_r(date("H:i:s"));
+      print_r("[".$user->getName()."] : ");
+      print_r($message);
+      print_r("\n");
     
    }
 }
